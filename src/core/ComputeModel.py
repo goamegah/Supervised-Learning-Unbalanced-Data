8e63ce679a3a30548131d868e1040d2ce8a82f89
Komlan Godwin AMEGAH <winneramegah492@gmail.com>
2023-03-20 15:13:21 +0100

    move module in core pkg

--- src/core/ComputeModel.py
+++ src/core/ComputeModel.py
@@ -4,95 +4,113 @@ from matplotlib.axes import Axes
 from sklearn.model_selection import train_test_split, StratifiedKFold
 from src.core.Processing import Processing
 from src.core.Model import Model
-from sklearn.model_selection import GridSearchCV,RandomizedSearchCV
-from src.globals import PARAMS_GRID
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+from src.core.globals import PARAMS_GRID
+
 
 class ComputeModel:
 
     def __init__(
-                self,df_X:pd.DataFrame,y:pd.Series,\
-                 model_name:str,process:Processing,hyperparameters:dict=None,
-                 l_order:list=None,positive_mod=None,
-                 grid_search=False,random_search=False,stratify=True,
-                 params_grid=None,cv=3,verbose=1
-                 ):
-        self.df_X=df_X
-        self.y=y
-        self.search=grid_search or random_search
-        self.stratify=stratify
+            self,
+            df_X: pd.DataFrame,
+            y: pd.Series,
+            model_name: str,
+            process: Processing,
+            hyperparameters: dict = None,
+            l_order: list = None,
+            positive_mod=None,
+            grid_search=False,
+            random_search=False,
+            stratify=True,
+            params_grid=None,
+            cv=3,
+            verbose=1
+    ):
+        self.df_X = df_X
+        self.y = y
+        self.search = grid_search or random_search
+        self.stratify = stratify
         if not self.search:
-            self.model=Model(model_name,hyperparameters=hyperparameters)
+            self.model = Model(model_name, hyperparameters=hyperparameters)
         else:
-            self.params_grid=PARAMS_GRID[model_name] if params_grid==None else params_grid
+            self.params_grid = PARAMS_GRID[model_name] if params_grid is None else params_grid
             cv = cv if not stratify else StratifiedKFold(cv)
             if random_search:
-                search=RandomizedSearchCV(Model(model_name).model,param_distributions=self.params_grid,cv=cv,verbose=verbose)
+                search = RandomizedSearchCV(Model(model_name).model, param_distributions=self.params_grid, cv=cv,
+                                            verbose=verbose)
             else:
-                search=GridSearchCV(Model(model_name).model, self.params_grid, verbose=verbose, cv=cv)
-            self.model=Model(model_name,search=search)
-        self.process=process
-        self.l_order=l_order
-        modalities_y=pd.unique(self.y)
-        if positive_mod == None:
-            positive_mod=modalities_y[0]
-        if not positive_mod in modalities_y:
+                search = GridSearchCV(Model(model_name).model, self.params_grid, verbose=verbose, cv=cv)
+            self.model = Model(model_name, search=search)
+        self.process = process
+        self.l_order = l_order
+        modalities_y = pd.unique(self.y)
+        if positive_mod is None:
+            positive_mod = modalities_y[0]
+        if not (positive_mod in modalities_y):
             raise Exception(f"{positive_mod} should be modality of {y}")
-        self.positive_mod=positive_mod
+        self.positive_mod = positive_mod
 
-    def numerize(self,df:pd.DataFrame) -> dict:
-        if self.l_order != None:
-            result=self.process.df_to_numerical(df,l_order=self.l_order)
-            df=result["df_transform"]
-            mapping=result["mapping"]
-            return {"df_transform":df,"mapping":mapping}
+    def numerize(self, df: pd.DataFrame) -> dict:
+        if self.l_order is not None:
+            result = self.process.df_to_numerical(df, l_order=self.l_order)
+            df = result["df_transform"]
+            mapping = result["mapping"]
+            return {"df_transform": df, "mapping": mapping}
         else:
-            df=self.process.df_to_numerical(df,l_order=self.l_order)["df_transform"]
-            return {"df_transform":df}
+            df = self.process.df_to_numerical(df, l_order=self.l_order)["df_transform"]
+            return {"df_transform": df}
 
-
-    def split(self,test_size=.2,random_state=42) ->None:
-        df_to_np=self.numerize(self.df_X)
-        X=np.array(df_to_np["df_transform"])
-        mapping=df_to_np["mapping"] if "mapping" in df_to_np.keys() else None
-        y=np.array(self.y == self.positive_mod).reshape(-1)
-        stratify=y if self.stratify else None
-        X_train,X_test,y_train,y_test=train_test_split(X,y, test_size=test_size, random_state=random_state,stratify=stratify)
+    def split(self, test_size=.2, random_state=42) -> None:
+        df_to_np = self.numerize(self.df_X)
+        X = np.array(df_to_np["df_transform"])
+        mapping = df_to_np["mapping"] if "mapping" in df_to_np.keys() else None
+        y = np.array(self.y == self.positive_mod).reshape(-1)
+        stratify = y if self.stratify else None
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state,
+                                                            stratify=stratify)
         if self.l_order:
-            self.dict_split={
-                "arrays":{"X_train":X_train,"y_train":y_train.reshape(-1),"X_test":X_test,"y_test":y_test.reshape(-1)},
-                "columns_features":df_to_np["df_transform"].columns,
-                "mapping":mapping
+            self.dict_split = {
+                "arrays": {
+                    "X_train": X_train,
+                    "y_train": y_train.reshape(-1),
+                    "X_test": X_test,
+                    "y_test": y_test.reshape(-1)
+                },
+                "columns_features": df_to_np["df_transform"].columns,
+                "mapping": mapping
             }
         else:
-            self.dict_split={
-                "arrays":{"X_train":X_train,"y_train":y_train.reshape(-1),"X_test":X_test,"y_test":y_test.reshape(-1)},
-                "columns_features":df_to_np["df_transform"].columns
-            }
+            self.dict_split = \
+                {
+                    "arrays": {
+                        "X_train": X_train,
+                        "y_train": y_train.reshape(-1),
+                        "X_test": X_test,
+                        "y_test": y_test.reshape(-1)
+                    },
+                    "columns_features": df_to_np["df_transform"].columns
+                }
 
-
-    def fit(self,test_size=.2,random_state=42)-> None:
-        self.split(test_size,random_state)
-        X_train=self.dict_split["arrays"]["X_train"]
-        y_train=self.dict_split["arrays"]["y_train"]
+    def fit(self, test_size=.2, random_state=42) -> None:
+        self.split(test_size, random_state)
+        X_train = self.dict_split["arrays"]["X_train"]
+        y_train = self.dict_split["arrays"]["y_train"]
         if not self.search:
-            self.model.fit(X_train,y_train)
+            self.model.fit(X_train, y_train)
         else:
-            self.model.search.fit(X_train,y_train)
+            self.model.search.fit(X_train, y_train)
             self.model.update()
 
-    def metrics(self,plot_roc=False,ax:Axes=None):
-        X,y=self.dict_split["arrays"]["X_test"],self.dict_split["arrays"]["y_test"]
-        return self.model.metrics(X,y.reshape(-1),plot_roc=plot_roc,ax=ax)
-        
-    def predict_proba(self,df:pd.DataFrame):
+    def metrics(self, plot_roc=False, ax: Axes = None):
+        X, y = self.dict_split["arrays"]["X_test"], self.dict_split["arrays"]["y_test"]
+        return self.model.metrics(X, y.reshape(-1), plot_roc=plot_roc, ax=ax)
+
+    def predict_proba(self, df: pd.DataFrame):
         return self.model.predict_proba(np.array(self.numerize(df)["df_transform"]))
 
-    def permutation_importance_model(self,scoring="roc_auc"):
-        return self.model.permutation_importance_model(self.dict_split["arrays"]["X_train"],\
-                                                       self.dict_split["arrays"]["y_train"],scoring)
-
-
-
-
-
-
+    def permutation_importance_model(self, scoring="roc_auc"):
+        return self.model.permutation_importance_model(
+            self.dict_split["arrays"]["X_train"],
+            self.dict_split["arrays"]["y_train"],
+            scoring
+        )
--- src/core/Model.py
+++ src/core/Model.py
@@ -3,19 +3,19 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.metrics import precision_recall_fscore_support, roc_curve, auc
-from src.globals import MODEL_HYPERPARAMETERS_DEF
-from sklearn.model_selection import GridSearchCV,RandomizedSearchCV
+from src.core.globals import MODEL_HYPERPARAMETERS_DEF
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.inspection import permutation_importance
 
 
-
 class Model:
 
-    def __init__(self, model_name: str, hyperparameters: dict = None, search: GridSearchCV or RandomizedSearchCV=None):
+    def __init__(self, model_name: str, hyperparameters: dict = None,
+                 search: GridSearchCV or RandomizedSearchCV = None):
         self.model_name = model_name
-        if search == None:
-            if hyperparameters == None:
+        if search is None:
+            if hyperparameters is None:
                 hyperparameters = MODEL_HYPERPARAMETERS_DEF[self.model_name]
             self.hyperparameters = hyperparameters
             if model_name == "Logistic Regression":
@@ -25,18 +25,18 @@ class Model:
             elif model_name == "DecisionTreeClassifier":
                 self.model = DecisionTreeClassifier(**self.hyperparameters)
             elif model_name == "RandomForestClassifier":
-                self.model=RandomForestClassifier(**self.hyperparameters)
+                self.model = RandomForestClassifier(**self.hyperparameters)
         else:
-            self.model=None
-            self.hyperparameters=None
-            self.search=search
+            self.model = None
+            self.hyperparameters = None
+            self.search = search
 
     def update(self):
         """
         :return: update model only for GridSearchCV
         """
-        self.model=self.search.best_estimator_
-        self.hyperparameters=self.search.best_params_
+        self.model = self.search.best_estimator_
+        self.hyperparameters = self.search.best_params_
 
     def fit(self, X, y):
         self.model.fit(X, y)
@@ -61,11 +61,12 @@ class Model:
         return self.model.predict(X)
 
     def metrics(self, X, y, plot_roc=False, ax: Axes = False):
-        precision, recall, f1_score, support = precision_recall_fscore_support(y, self.predict(X), average="binary",pos_label=True)
+        precision, recall, f1_score, support = precision_recall_fscore_support(y, self.predict(X), average="binary",
+                                                                               pos_label=True)
         fpr, tpr, thresholds = roc_curve(y, self.predict_proba(X))
         auc_value = auc(fpr, tpr)
         if plot_roc:
-            if ax == None:
+            if ax is None:
                 raise Exception(f"{ax} for plotting roc curve is None")
             ax.plot(fpr, tpr)
             ax.set_ylabel('True Positive Rate')
@@ -79,10 +80,9 @@ class Model:
             "f1_score": f1_score
         }
 
-
     def plot_model(self):
         if self.model_name == "DecisionTreeClassifier":
             return plot_tree(self.model)
 
-    def permutation_importance_model(self,X,y,scoring):
+    def permutation_importance_model(self, X, y, scoring):
         return permutation_importance(self.model, X, y, scoring=scoring)
